da2ade793b0a6e646a173c9ce30ee0b1bd0885ef
pavan511999 <naperupavan576@gmail.com>
2020-10-15 14:59:12 +0530

    3rd commit

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hi");
+        Customer c=new Customer("Pavan","Mumbai");
+        System.out.println(c);
 	}
-
 }
